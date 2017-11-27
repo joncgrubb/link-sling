a4f245b070443e198a7113febae6b8adf24d28e9
joncgrubb <joncgrubbdev@gmail.com>
2017-11-27 14:04:11 -0500

    add resource controller for messages

--- routes/web.php
+++ routes/web.php
@@ -21,6 +21,8 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
+Route::resource('/message', 'MessageController');
+
 Route::match(array('GET', 'POST'), '/incoming', function()
 {
   $twiml = new Twilio\Twiml();
