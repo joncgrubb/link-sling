48483dc9e22215d2e722d73cb899fe862cd51166
joncgrubb <joncgrubbdev@gmail.com>
2017-11-27 10:07:01 -0500

    add messages table migration

---
+++ database/migrations/2017_11_27_145100_create_messages_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sender_id')->unsigned();
+            $table->foreign('sender_id')->references('id')->on('users')->onDelete('cascade');
+            $table->tinyInteger('mobile');
+            $table->string('link', 250);
+            $table->datetime('sent_at');
+            $table->boolean('is_received')->default(false);
+            $table->boolean('is_deleted')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
