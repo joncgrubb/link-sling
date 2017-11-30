b553f9002ffe9c91d0e4b352c7ae21102ef5a1f4
joncgrubb <joncgrubbdev@gmail.com>
2017-11-30 14:15:05 -0500

    removed x close button from History modals to match other modals

--- resources/views/messages/history.blade.php
+++ resources/views/messages/history.blade.php
@@ -32,7 +32,6 @@
                                 <div class="modal-dialog">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
                                             <h4 class="modal-title">{{ $contacts->where('mobile', $message->mobile)->pluck('name')->first() }}</h4>
                                             <h4 class="modal-title modal-mobile">{{ $message->mobile }}</h4>
                                         </div>
