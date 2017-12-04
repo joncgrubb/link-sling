f00d9feded1107f13b647e8ff6293674e2826985
joncgrubb <joncgrubbdev@gmail.com>
2017-12-04 18:54:50 -0500

    added twilio brand

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,6 @@
-<div id="copyright" style="display: inline-block; color: #ccc; bottom: 0; margin-top: 100px;">© Copyright 2017 Link-Sling</div>
+<div id="copyright" style="display: inline-block; color: #ccc; bottom: 0; margin-top: 100px;">
+	© Copyright 2017 Link-Sling
+</div>
+<div class="text-center">
+	<img style="display: inline-block; height: 15%; width: 15%;" src="{{ asset('twilio.png') }}">
+</div>
