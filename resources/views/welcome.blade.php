fff69f9babea52be8f488e212bb50704c2824fff
joncgrubb <joncgrubbdev@gmail.com>
2017-12-05 12:59:08 -0500

    reworded how to, added media breakpoints for how to columns

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,22 +11,22 @@
     </div>
     <h3 class="text-center how-does">How does it work?</h3>
     <div class="row how-to equal">
-    	<div class="col-xs-4">
+    	<div class="col-xs-12 col-md-4">
       	<div class="panel panel-default text-center how-to-panel">
       		<i class="fa fa-desktop fa-4x how-to-icon" aria-hidden="true"></i>
-      		<p class="how-to-panel-text">Search the web and find an awesome link!</p>
+      		<p class="how-to-panel-text">Search the web and find an awesome link that you want to share!</p>
       	</div>
       </div>
-      <div class="col-xs-4">
+      <div class="col-xs-12 col-md-4">
       	<div class="panel panel-default text-center how-to-panel">
       		<i class="fa fa-link fa-4x how-to-icon" aria-hidden="true"></i>
       		<p class="how-to-panel-text">Copy/Paste the link and <b style="color: #4CA6FF; text-shadow: 1px 1px 1px #ccc;">Sling!</b> to your saved contacts!</p>
       	</div>
       </div>
-      <div class="col-xs-4">
+      <div class="col-xs-12 col-md-4">
       	<div class="panel panel-default text-center how-to-panel">
       		<i class="fa fa-mobile fa-4x how-to-icon" aria-hidden="true"></i>
-      		<p class="how-to-panel-text">Your contacts receive the link via SMS!</p>
+      		<p class="how-to-panel-text">After accepting messages from us, your contacts receive the link via SMS!</p>
       	</div>
       </div>
     </div>
