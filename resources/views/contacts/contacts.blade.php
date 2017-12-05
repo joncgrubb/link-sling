4bb1128edc8811807a083afadf9a36b05378a193
joncgrubb <joncgrubbdev@gmail.com>
2017-12-05 13:50:37 -0500

    added top margin to all main panels once logged in

--- resources/views/contacts/contacts.blade.php
+++ resources/views/contacts/contacts.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+            <div class="panel panel-default sling-panel">
                 <div class="panel-heading sling-main-panel">Link-Sling</div>
                 <div class="panel-heading text-center"><h4>Contacts</h4></div>
 
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+            <div class="panel panel-default sling-panel">
                 <div class="panel-heading sling-main-panel">Link-Sling</div>
 
                 <div class="panel-body">
